9e5ae82d839531d4f13a70f8374f13efabf4cab5
romz <romz@wp.pl>
2026-08-30 22:05:28 +0200

    Add evaluation of eigenvalues for Coulomb potential

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,72 @@
 #include <iostream>
+#include "eigprob.h"
+#include "gauss.h"
+#include "lobatto.h"
+#include "paramdb.h"
+#include "potential_coulomb.h"
 
-int main()
+static void Intro(FILE* out);
+
+
+int main(int argc, char* argv[])
 {
+    Intro(stdout);
+
+    // argc = 2;
+    // argv[ 1 ] = "/home/romz/code/eigenrad-sequential/results/aaa.inp";
+
+
+    if(argc != 2)
+    {
+        printf("Usage: eigenrad <input_file>\n\n");
+        return 1;
+    }
+
+    try
+    {
+        Lobatto m_lobatto;
+        Gauss m_gauss;
+
+        ParamDb db( argv[ 1 ] );
+        potential_coulomb pot;
+        const size_t ell = ParamDb::GetSize_t( "Solver_Ell" );
+        const size_t eigNo = ParamDb::GetSize_t( "Solver_EigNo" );
+        EigProb eigProb( ell );
+        eigProb.SolveAdapt( pot, eigNo);
+
+        for(size_t eig = 0; eig < eigNo; eig++ )
+            std::cout << eigProb.GetEigVal(eig) << "\n";
+
+
+        printf("\n\n********** CALCULATIONS FINISHED SUCCESSFULLY! **********\n\n\n");
+        return 0;
+    }
+    catch( std::exception& e )
+    {
+        printf("\n\nERROR! %s\n\n\n", e.what());
+        return 1;
+    }
+    catch( ... )
+    {
+        printf( "Unexpected error!\n\n" );
+        return 1;
+    }
+
+
     std::cout << "EIGENRAD\n";
     return 0;
 }
+
+void Intro(FILE* out)
+{
+    fprintf(out,
+        "===============================================================================\n"
+        " EEEEE I   GGG  EEEEE N   N RRRR   AAA  DDDD         Zbigniew Romanowski       \n"
+        " E     I  G     E     NN  N R   R A   A D   D                                  \n"
+        " EEE   I  G  GG EEE   N N N RRRR  AAAAA D   D        romz@wp.pl                \n"
+        " E     I  G   G E     N  NN R  R  A   A D   D                                  \n"
+        " EEEEE I   GGG  EEEEE N   N R   R A   A DDDD         https://github.com/romz-pl\n"
+        "===============================================================================\n\n\n");
+}
+
+
---
+++ src/potential_coulomb.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cassert>
+#include "fun1D.h"
+
+class potential_coulomb : public Fun1D
+{
+public:
+    potential_coulomb() { }
+    virtual ~potential_coulomb() { }
+
+    virtual double Get(double r) const
+    {
+        assert( r > 0 );
+        return -1.0 / r;
+    };
+};
