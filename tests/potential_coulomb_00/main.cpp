e78905af6ec14ff466aecdbd36b2f939a1959a49
romz <romz@wp.pl>
2026-08-31 08:35:40 +0200

    Add test for Coulomb potential for ell=1,2,3,4

--- tests/potential_coulomb_00/main.cpp
+++ tests/potential_coulomb_00/main.cpp
@@ -21,9 +21,125 @@ TEST(coulomb, ell_0_eigno_4)
 
     const size_t ell = 0;
     const size_t eigNo = 4;
-    const double rc      = 100.0;
+    const double rc = 80.0;
     const size_t eigNode = 4;
-    const size_t eigDeg  = 6;
+    const size_t eigDeg = 6;
+    const double abstol = 1E-14;
+    const double absMaxCoef = 1E-4;
+
+
+    EigProb eigProb( ell, rc, eigNode, eigDeg );
+    ASSERT_NO_THROW( eigProb.SolveAdapt( pot, eigNo, absMaxCoef, abstol ) );
+
+
+    const double abs_error = 1E-6;
+    for(size_t n = 0; n < eigNo; n++ )
+    {
+        const double v0 = eigProb.GetEigVal( n );
+        const double v1 = eigen_value( ell, n );
+        EXPECT_NEAR(v0, v1, abs_error);
+    }
+
+}
+
+TEST(coulomb, ell_1_eigno_4)
+{
+    Lobatto m_lobatto;
+    Gauss m_gauss;
+    potential_coulomb pot;
+
+    const size_t ell = 1;
+    const size_t eigNo = 4;
+    const double rc = 100.0;
+    const size_t eigNode = 4;
+    const size_t eigDeg = 6;
+    const double abstol = 1E-14;
+    const double absMaxCoef = 1E-4;
+
+
+    EigProb eigProb( ell, rc, eigNode, eigDeg );
+    ASSERT_NO_THROW( eigProb.SolveAdapt( pot, eigNo, absMaxCoef, abstol ) );
+
+
+    const double abs_error = 1E-6;
+    for(size_t n = 0; n < eigNo; n++ )
+    {
+        const double v0 = eigProb.GetEigVal( n );
+        const double v1 = eigen_value( ell, n );
+        EXPECT_NEAR(v0, v1, abs_error);
+    }
+
+}
+
+TEST(coulomb, ell_2_eigno_4)
+{
+    Lobatto m_lobatto;
+    Gauss m_gauss;
+    potential_coulomb pot;
+
+    const size_t ell = 2;
+    const size_t eigNo = 4;
+    const double rc = 140.0;
+    const size_t eigNode = 4;
+    const size_t eigDeg = 6;
+    const double abstol = 1E-14;
+    const double absMaxCoef = 1E-4;
+
+
+    EigProb eigProb( ell, rc, eigNode, eigDeg );
+    ASSERT_NO_THROW( eigProb.SolveAdapt( pot, eigNo, absMaxCoef, abstol ) );
+
+
+    const double abs_error = 1E-6;
+    for(size_t n = 0; n < eigNo; n++ )
+    {
+        const double v0 = eigProb.GetEigVal( n );
+        const double v1 = eigen_value( ell, n );
+        EXPECT_NEAR(v0, v1, abs_error);
+    }
+
+}
+
+TEST(coulomb, ell_3_eigno_4)
+{
+    Lobatto m_lobatto;
+    Gauss m_gauss;
+    potential_coulomb pot;
+
+    const size_t ell = 3;
+    const size_t eigNo = 4;
+    const double rc = 160.0;
+    const size_t eigNode = 4;
+    const size_t eigDeg = 6;
+    const double abstol = 1E-14;
+    const double absMaxCoef = 1E-4;
+
+
+    EigProb eigProb( ell, rc, eigNode, eigDeg );
+    ASSERT_NO_THROW( eigProb.SolveAdapt( pot, eigNo, absMaxCoef, abstol ) );
+
+
+    const double abs_error = 1E-6;
+    for(size_t n = 0; n < eigNo; n++ )
+    {
+        const double v0 = eigProb.GetEigVal( n );
+        const double v1 = eigen_value( ell, n );
+        EXPECT_NEAR(v0, v1, abs_error);
+    }
+
+}
+
+TEST(coulomb, ell_4_eigno_4)
+{
+    Lobatto m_lobatto;
+    Gauss m_gauss;
+    potential_coulomb pot;
+
+    const size_t ell = 4;
+    const size_t eigNo = 4;
+    const double rc = 180.0;
+    const size_t eigNode = 4;
+    const size_t eigDeg = 6;
     const double abstol = 1E-14;
     const double absMaxCoef = 1E-4;
 
