1087ed26fe4a32121360764a954281ccb2cdaddf
romz <romz@wp.pl>
2026-09-10 08:59:19 +0200

    Use multiplication by 0.5 instead of division by 2

--- src/element.cpp
+++ src/element.cpp
@@ -28,8 +28,8 @@ void Element::Set( double x0, double x1, size_t p )
     assert( x1 > x0 );
 
     // Calculates the transformation coefficients form interval $[x_m, x_{m+1}]$ to reference interval $[-1, 1]$.
-    m_c1 = ( x1 + x0 ) / 2;
-    m_c2 = ( x1 - x0 ) / 2;
+    m_c1 = 0.5 * ( x1 + x0 );
+    m_c2 = 0.5 * ( x1 - x0 );
 
     m_dof.resize( p + 1 );
 }
