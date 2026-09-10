cfb343aa48b334e96eda5243cdced71350198ffa
romz <romz@wp.pl>
2026-09-10 19:04:14 +0200

    Remove no longer needed class [Bndr]

--- src/bndr.h
+++ src/bndr.h
@@ -19,20 +19,3 @@ enum BndrType
     BndrType_Neu // Neumann
 };
 
-
-class Bndr
-{
-public:
-    Bndr() : m_type( BndrType_Emp ), m_val( 0 ) { }
-
-    Bndr( BndrType type, double val ) : m_type( type ), m_val( val ) { }
-
-    ~Bndr() = default; 
-
-public:
-    // Type of boundary condition
-    BndrType m_type;
-
-    // Value of boundary contition
-    double m_val;
-};
