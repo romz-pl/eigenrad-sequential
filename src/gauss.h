66944e8c5af3e0308a90cf5ee22395e5e119c6ec
romz <romz@wp.pl>
2026-09-11 08:00:19 +0200

    Remove static functions in class Gauss and use C++23 idioms

--- src/gauss.h
+++ src/gauss.h
@@ -3,29 +3,27 @@
 //
 // Gauss quadrature
 //
-// The quadrature weights are stored in the array $w$.
-// The quadrature node coordinates are stored in the array $x$.
-//
 
-#include <cassert>
 #include <vector>
-
+#include <span>
 
 class Gauss
 {
 public:
-    Gauss(size_t deg);
-    ~Gauss() = default;
+    explicit Gauss(size_t deg);
 
-    static double X( size_t i )  { return m_x[ i ]; }
-    static double W( size_t i )  { return m_w[ i ]; }
-    static size_t Size( )  { assert(m_x.size() == m_w.size()); return m_x.size(); }
+    Gauss(const Gauss&)            = default;
+    Gauss(Gauss&&)                 = default;
+    Gauss& operator=(const Gauss&) = default;
+    Gauss& operator=(Gauss&&)      = default;
+    ~Gauss()                       = default;
 
+    [[nodiscard]] std::span<const std::pair<double, double>> Nodes() const noexcept
+    {
+        return m_xw;
+    }
 
 private:
-    // Quadrature weights
-    static std::vector< double > m_w;
-
-    // Quadrature nodes
-    static std::vector< double > m_x;
+    // The quadrature node coordinates, and weights are stored in this array
+    std::vector<std::pair<double, double>> m_xw;
 };
